1b9d42b526ef26f6ab2d96bf2d5cc297c9e0e8ae
k-84mo10 <kokiaptx0812@gmail.com>
2025-04-21 17:18:17 +0900

    add hankaku.txt

---
+++ myOS/kernel/.font.d
@@ -0,0 +1,5 @@
+font.o: font.cpp font.hpp \
+  /home/kohki_mikan/osbook/devenv/x86_64-elf/include/c++/v1/cstdint \
+  /home/kohki_mikan/osbook/devenv/x86_64-elf/include/c++/v1/__config \
+  /home/kohki_mikan/osbook/devenv/x86_64-elf/include/c++/v1/stdint.h \
+  graphics.hpp frame_buffer_config.hpp
