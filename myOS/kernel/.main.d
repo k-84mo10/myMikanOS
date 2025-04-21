bf306b8ecf53e9d4d4e36327d57371ce0e79a3d1
k-84mo10 <kokiaptx0812@gmail.com>
2025-04-21 20:53:53 +0900

    make terminal

--- myOS/kernel/.main.d
+++ myOS/kernel/.main.d
@@ -4,4 +4,6 @@ main.o: main.cpp \
   /home/kohki_mikan/osbook/devenv/x86_64-elf/include/c++/v1/stdint.h \
   /home/kohki_mikan/osbook/devenv/x86_64-elf/include/c++/v1/cstddef \
   /home/kohki_mikan/osbook/devenv/x86_64-elf/include/c++/v1/version \
-  frame_buffer_config.hpp graphics.hpp font.hpp
+  /home/kohki_mikan/osbook/devenv/x86_64-elf/include/c++/v1/cstdio \
+  /home/kohki_mikan/osbook/devenv/x86_64-elf/include/c++/v1/stdio.h \
+  frame_buffer_config.hpp graphics.hpp font.hpp console.hpp
