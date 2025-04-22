48fc37eab991099e309d37a4bdc25df2fbd8abdd
k-84mo10 <kokiaptx0812@gmail.com>
2025-04-22 23:54:00 +0900

    enable pci controller

--- myOS/kernel/.main.d
+++ myOS/kernel/.main.d
@@ -6,4 +6,32 @@ main.o: main.cpp \
   /home/kohki_mikan/osbook/devenv/x86_64-elf/include/c++/v1/version \
   /home/kohki_mikan/osbook/devenv/x86_64-elf/include/c++/v1/cstdio \
   /home/kohki_mikan/osbook/devenv/x86_64-elf/include/c++/v1/stdio.h \
-  frame_buffer_config.hpp graphics.hpp font.hpp console.hpp
+  frame_buffer_config.hpp graphics.hpp font.hpp console.hpp pci.hpp \
+  /home/kohki_mikan/osbook/devenv/x86_64-elf/include/c++/v1/array \
+  /home/kohki_mikan/osbook/devenv/x86_64-elf/include/c++/v1/__tuple \
+  /home/kohki_mikan/osbook/devenv/x86_64-elf/include/c++/v1/type_traits \
+  /home/kohki_mikan/osbook/devenv/x86_64-elf/include/c++/v1/utility \
+  /home/kohki_mikan/osbook/devenv/x86_64-elf/include/c++/v1/initializer_list \
+  /home/kohki_mikan/osbook/devenv/x86_64-elf/include/c++/v1/cstring \
+  /home/kohki_mikan/osbook/devenv/x86_64-elf/include/c++/v1/string.h \
+  /home/kohki_mikan/osbook/devenv/x86_64-elf/include/c++/v1/__debug \
+  /home/kohki_mikan/osbook/devenv/x86_64-elf/include/c++/v1/iterator \
+  /home/kohki_mikan/osbook/devenv/x86_64-elf/include/c++/v1/iosfwd \
+  /home/kohki_mikan/osbook/devenv/x86_64-elf/include/c++/v1/wchar.h \
+  /home/kohki_mikan/osbook/devenv/x86_64-elf/include/c++/v1/__functional_base \
+  /home/kohki_mikan/osbook/devenv/x86_64-elf/include/c++/v1/typeinfo \
+  /home/kohki_mikan/osbook/devenv/x86_64-elf/include/c++/v1/exception \
+  /home/kohki_mikan/osbook/devenv/x86_64-elf/include/c++/v1/cstdlib \
+  /home/kohki_mikan/osbook/devenv/x86_64-elf/include/c++/v1/stdlib.h \
+  /home/kohki_mikan/osbook/devenv/x86_64-elf/include/c++/v1/new \
+  /home/kohki_mikan/osbook/devenv/x86_64-elf/include/c++/v1/algorithm \
+  /home/kohki_mikan/osbook/devenv/x86_64-elf/include/c++/v1/memory \
+  /home/kohki_mikan/osbook/devenv/x86_64-elf/include/c++/v1/limits \
+  /home/kohki_mikan/osbook/devenv/x86_64-elf/include/c++/v1/tuple \
+  /home/kohki_mikan/osbook/devenv/x86_64-elf/include/c++/v1/stdexcept \
+  /home/kohki_mikan/osbook/devenv/x86_64-elf/include/c++/v1/cassert \
+  /home/kohki_mikan/osbook/devenv/x86_64-elf/include/assert.h \
+  /home/kohki_mikan/osbook/devenv/x86_64-elf/include/_ansi.h \
+  /home/kohki_mikan/osbook/devenv/x86_64-elf/include/c++/v1/functional \
+  /home/kohki_mikan/osbook/devenv/x86_64-elf/include/c++/v1/bit \
+  error.hpp
